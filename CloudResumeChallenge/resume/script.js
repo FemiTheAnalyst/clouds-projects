0056c1480037190fc378b80fd0f6c2545f641533
femundo <femundo007@gmail.com>
2023-04-11 13:47:41 +0100

    file updates

--- CloudResumeChallenge/resume/script.js
+++ CloudResumeChallenge/resume/script.js
@@ -1,17 +1,3 @@
-// "use strict";
-
-// $(document).ready(() => {
-//     $.post('https://hw3w8zp4xc.execute-api.us-east-1.amazonaws.com/Prod/visit')
-//     .done(visitor_counter => {
-//         $('#loader').hide();
-//         $('#visits').text(visitor_counter);
-//     })
-//     .fail(e => {
-//         console.log('Error');
-//         console.log(e);
-//     });
-// });
-
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function() {
